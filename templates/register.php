2be46440c7dbd92981cb84c44ec24fc47a5cd6c7
Thomas LHTS <thomas.lhts.95@icloud.com>
2022-11-03 14:09:33 +0100

    :feat: updated register & login mechanism regarding to the user class

--- templates/register.php
+++ templates/register.php
@@ -4,7 +4,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/users.css" />
+    <link rel="stylesheet" href="css/main.css" />
   </head>
   <body>
     <div class="container">
@@ -12,7 +12,43 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
          Vivamus vehicula magna tortor, vitae porttitor ante dictum in. Quisque diam sapien.</p>
 
-      <!-- Login form -->
+      <?php
+      if(!empty($templateVars['register_success']) && $templateVars['register_success'] > 0)
+      {
+      ?>
+        <!-- Account has been created success -->
+        <div class="alert">
+          <div class="alert--success">
+            <span class="medium">Your account has been created, you can now login using the login form.</span>
+          </div> <!--/ .success -->
+        </div> <!-- ./alert -->
+
+      <?php
+      }
+      elseif(!empty($templateVars['register_success']) && $templateVars['register_success'] == -1)
+      {
+      ?>
+        <!-- Username already in use -->
+        <div class="alert">
+          <div class="alert--error">
+            <span class="medium">Username already in use, please pick another one.</span>
+          </div> <!-- /.error -->
+        </div> <!-- /.alert -->
+      <?php
+      }
+      elseif(!empty($templateVars['password_missmatch']) && $templateVars['password_missmatch'])
+      {
+      ?>
+        <!-- Password missmatch -->
+        <div class="alert">
+          <div class="alert--error">
+            <span class="medium">Password & password confirmation missmatch.</span>
+          </div> <!-- /.error -->
+        </div> <!-- /.alert -->
+      <?php
+      }
+      ?>
+      <!-- Register form -->
       <form method="POST">
         <label>Email address :</label>
         <input type="email" name="email" placeholder="Email address.." <?= !REGISTRATION_ENABLED ? 'disabled':'' ?>  />
