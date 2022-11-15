23f258e5d8e4c3aeca0d1af12fc25e07207ec21f
Thomas LHTS <thomas.lhts.95@icloud.com>
2022-11-15 13:40:49 +0100

    :feat: cheatsheets boilerplate

---
+++ classes/cheatsheet.php
@@ -0,0 +1,25 @@
+<?php
+class Cheatsheet
+{
+
+  protected $dbh;
+  protected $content;
+  protected $tags;
+
+  public function __construct(PDO $dbh)
+  {
+    $this->dbh = $dbh;
+  }
+
+  public function save()
+  {
+
+  }
+
+  public function fetch()
+  {
+
+  }
+
+}
+// EF
